cdf0106823b5c23d58551f7dbac9610e59d99cae
nayidob <nayidob@gmail.com>
2020-05-24 12:06:00 -0500

    Image Window
    Create the window to show image
    Use Object_info Dictionary to store string and image info variables

--- txteditor.py
+++ txteditor.py
@@ -25,10 +25,12 @@ en_stops = set(stopwords.words('english'))
 
 def main():
     filename_text_image = {}
-    set_environment(filename_text_image)
+    objects_info = {}
+    set_environment(filename_text_image, objects_info)
+    show_result(filename_text_image, objects_info)
 
 
-def set_environment(filename_text_image):
+def set_environment(filename_text_image, objects_info):
     canvas = make_canvas(CANVAS_WIDTH, CANVAS_HEIGHT, 'Secret Image')
     # animation loop
     canvas.rowconfigure(5, minsize=50, weight=1)
@@ -66,7 +68,7 @@ def set_environment(filename_text_image):
                                                  "\n to"
                                                  "\n ENCRYPT"
                                                  , fg="red", font="Calibri 12 bold",
-                                    command=lambda: encrypt(filename_text_image))
+                                    command=lambda: encrypt(filename_text_image, objects_info))
     button_open.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
     button_load.grid(row=2, column=0, sticky="ew", padx=5)
     button_encrypt.grid(row=5, column=4, sticky="nsew", padx=10, pady=10)
@@ -79,26 +81,71 @@ def set_environment(filename_text_image):
     canvas.mainloop()
 
 
-def encrypt(filename_text_image):
-    print(filename_text_image)
-    canvas_text = make_canvas(CANVAS_WIDTH // 5, CANVAS_HEIGHT // 5, 'Text File')
-    text_filename = (filename_text_image["text"])
-    count_words(text_filename, canvas_text)
-    main_string = create_string_from_file(text_filename)
-    length = count_char(text_filename, main_string, canvas_text)
+def show_result(filename_text_image, objects_info):
+    canvas_image = make_canvas(CANVAS_WIDTH, CANVAS_HEIGHT // 2, 'Resulting Image')
+    image_filename = (filename_text_image["image"])
+    tkinter.Message(canvas_image, text="\n"
+                                       "=================================="
+                                       "\n"
+                                       "\nThe dimensions of your original image"
+                                       " in pixels are:"
+                                       "\n"
+                                       "\n Width %s pixels"
+                                       "\n Height %s pixels" % (objects_info["width_original_image"],
+                                                                objects_info["height_original_image"]),
+                    width=300, justify=tkinter.CENTER).grid(row=0, column=0, sticky="ew")
+
+    tkinter.Message(canvas_image, text="\n"
+                                       "=================================="
+                                       "\n"
+                                       "\nThe dimensions of your resulting image"
+                                       " in pixels are:"
+                                       "\n"
+                                       "\n Width %s pixels"
+                                       "\n Height %s pixels" % (objects_info["size"][0],
+                                                                objects_info["size"][1]),
+                    width=300, justify=tkinter.CENTER).grid(row=1, column=0, sticky="ew")
+
+    original_image = ImageTk.PhotoImage(Image.open(image_filename))
+    image_label = tkinter.Label(canvas_image, image=original_image)
+    image_label.grid(row=0, column=1, sticky="ew", columnspan=2)
+
+    modified_image = ImageTk.PhotoImage(Image.open("Encrypted_Image_Text.png"))
+    image_label2 = tkinter.Label(canvas_image, image=modified_image)
+    image_label2.grid(row=1, column=1, sticky="ew", columnspan=2)
+
+    canvas_image.update()
+    canvas_image.mainloop()
+
+def load_and_prepare_image(filename_text_image, objects_info):
     image = Image.open(filename_text_image["image"]).convert("RGBA")
-    width_original_image, height_original_image = image.size
-    original_aspect_ratio = height_original_image / width_original_image
+    objects_info["width_original_image"], objects_info["height_original_image"] = image.size
+    original_aspect_ratio = objects_info["height_original_image"] / objects_info["width_original_image"]
     print(original_aspect_ratio)
-    size = get_size(length, original_aspect_ratio)
-    print(size)
-    final_image = image.resize((size[0], size[1]))
-    final_image = draw_text(main_string, final_image, size)
-    final_image.save("Test.png")
-    final_image.show()
-    dict_words = get_counts_dict(text_filename)
-    s = sorted(dict_words.items(), key=operator.itemgetter(1), reverse=True)
-    print(s[0:20])
+    objects_info["size"] = get_size(objects_info["length"], original_aspect_ratio)
+    print(objects_info["size"])
+    final_image = image.resize((objects_info["size"][0], objects_info["size"][1]))
+    return final_image
+
+
+def encrypt(filename_text_image, objects_info):
+    show_text_summary(filename_text_image, objects_info)
+    final_image = load_and_prepare_image(filename_text_image, objects_info)
+    final_image = put_text_within_image(objects_info["main_string"], final_image, objects_info["size"])
+    final_image.save("Encrypted_Image_Text.png")
+
+
+
+def show_text_summary(filename_text_image, string_info):
+    canvas_text = make_canvas(CANVAS_WIDTH // 2, CANVAS_HEIGHT // 4, 'Information About Text File')
+    text_filename = (filename_text_image["text"])
+
+    count_words(text_filename, canvas_text)
+    string_info["main_string"] = create_string_from_file(text_filename)
+
+    string_info["length"] = count_char(text_filename, string_info["main_string"], canvas_text)
+    frequency_of_20_top_words(text_filename, canvas_text)
+
 
 def open_file(filename_text_image, txt_edit):
     """Open a file for editing."""
@@ -197,11 +244,24 @@ def create_string_from_file(text_filename):
                 count += 1
     return text_file
 
+def frequency_of_20_top_words(text_filename, canvas_text):
+    dict_words = get_counts_dict(text_filename)
+    s = sorted(dict_words.items(), key=operator.itemgetter(1), reverse=True)
+    tkinter.Message(canvas_text, text="\n"
+                                      "=================================="
+                                      "\n"
+                                      "\nThese are the TOP 20 words in the text"
+                                      " and their frequency: ('word', freq)"
+                                      "\n"
+                                      "\n %s" % s[0:20], width=300,
+                    justify=tkinter.CENTER).grid(row=3, column=0, sticky="ew")
+    print(s[0:20])
+
 
 def count_char(text_filename, main_string, canvas_text):
     char_count = len(main_string)
     tkinter.Message(canvas_text, text="%s characters" % char_count,
-                    width=200, fg="blue",
+                    width=200, fg="blue", font="Calibri 16 bold",
                     justify=tkinter.CENTER).grid(row=2, column=0, sticky="ew")
     print(text_filename + " contains " + str(char_count) + " characters")
     return char_count
@@ -222,15 +282,15 @@ def count_words(filename, canvas_text):
     tkinter.Message(canvas_text, text="The text selected: "
                                       "\n %s"
                                       "\n contains:" % filename,
-                    width=200,
+                    width=200, font="Calibri 14 bold",
                     justify=tkinter.CENTER).grid(row=0, column=0, sticky="ew")
     tkinter.Message(canvas_text, text="%s words" % count,
-                    width=200, fg="blue",
+                    width=200, fg="blue", font="Calibri 16 bold",
                     justify=tkinter.CENTER).grid(row=1, column=0, sticky="ew")
     print(filename + " contains " + str(count) + " words")
 
 
-def draw_text(main_string, final_image, size):
+def put_text_within_image(main_string, final_image, size):
     count = 0
     width, height = final_image.size
     pixels = final_image.load()
